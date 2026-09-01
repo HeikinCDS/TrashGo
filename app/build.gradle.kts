c8e74a6573c873be75495bb264f573f1500eb573
HeikinCDS <chinyipsho@gmail.com>
2026-09-01 20:11:56 +0800

    Fix Gradle runtime and Android test dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -68,7 +68,10 @@ dependencies {
     implementation("com.google.android.gms:play-services-maps:19.0.0")
     implementation("com.google.android.gms:play-services-location:21.3.0")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
+
     testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.3.0")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.7.0")
 
     implementation("androidx.exifinterface:exifinterface:1.3.7")
     implementation("com.journeyapps:zxing-android-embedded:4.3.0")
